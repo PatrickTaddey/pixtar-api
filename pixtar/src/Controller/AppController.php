6bd3eb494592aaf287fd5a2181c9cc746279dba7
tooltonix <patrick@tubenschleim.de>
2015-06-10 17:40:30 +0200

    Make the default API configuration available to all controllers in your application

--- pixtar/src/Controller/AppController.php
+++ pixtar/src/Controller/AppController.php
@@ -15,6 +15,7 @@
 namespace App\Controller;
 
 use Cake\Controller\Controller;
+use Cake\Event\Event;
 
 /**
  * Application Controller
@@ -24,19 +25,58 @@ use Cake\Controller\Controller;
  *
  * @link http://book.cakephp.org/3.0/en/controllers.html#the-app-controller
  */
-class AppController extends Controller
-{
+class AppController extends Controller {
 
-    /**
-     * Initialization hook method.
-     *
-     * Use this method to add common initialization code like loading components.
-     *
-     * @return void
-     */
-    public function initialize()
-    {
-        parent::initialize();
-        $this->loadComponent('Flash');
-    }
+	use \Crud\Controller\ControllerTrait;
+
+	public $components = [
+		'RequestHandler',
+		'Crud.Crud' => [
+			'actions' => [
+				'Crud.Index',
+				'Crud.View',
+				'Crud.Add',
+				'Crud.Edit',
+				'Crud.Delete',
+			],
+			'listeners' => [
+				'Crud.Api',
+				'Crud.ApiPagination',
+				'Crud.ApiQueryLog',
+			],
+		],
+	];
+
+	/**
+	 * Initialization hook method.
+	 *
+	 * Use this method to add common initialization code like loading components.
+	 *
+	 * @return void
+	 */
+	public function initialize() {
+		parent::initialize();
+		$this->loadComponent('Flash');
+	}
+
+	/**
+	 * overwrite beforeFilter to enable CORS
+	 *
+	 * @param Event $event
+	 * @return void
+	 */
+	public function beforeFilter(Event $event) {
+
+		/* allow all origins */
+		$allowed_origin = $this->request->header("Origin");
+		header("Access-Control-Allow-Origin: " . $allowed_origin);
+		header("Access-Control-Allow-Credentials: true");
+		header("Access-Control-Allow-Headers: X-CSRF-Token,X-Requested-With,Content-Type");
+
+		/* handle options */
+		if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
+			header("Access-Control-Allow-Methods: GET,PUT,POST,DELETE,OPTIONS");
+			exit;
+		}
+	}
 }
