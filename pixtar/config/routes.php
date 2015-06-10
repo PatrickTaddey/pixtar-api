3ee74c99ee57b23738f5e3ca1fbee7cddadc107b
tooltonix <patrick@tubenschleim.de>
2015-06-10 17:39:34 +0200

    register routes '/users' + '/images'

--- pixtar/config/routes.php
+++ pixtar/config/routes.php
@@ -41,36 +41,46 @@ use Cake\Routing\Router;
  */
 Router::defaultRouteClass('Route');
 
+/* enable extensions */
+Router::extensions(['json', 'xml']);
+
 Router::scope('/', function ($routes) {
-    /**
-     * Here, we are connecting '/' (base path) to a controller called 'Pages',
-     * its action called 'display', and we pass a param to select the view file
-     * to use (in this case, src/Template/Pages/home.ctp)...
-     */
-    $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+	/**
+	 * Here, we are connecting '/' (base path) to a controller called 'Pages',
+	 * its action called 'display', and we pass a param to select the view file
+	 * to use (in this case, src/Template/Pages/home.ctp)...
+	 */
+	$routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
 
-    /**
-     * ...and connect the rest of 'Pages' controller's URLs.
-     */
-    $routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
+	/**
+	 * ...and connect the rest of 'Pages' controller's URLs.
+	 */
+	$routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
 
-    /**
-     * Connect catchall routes for all controllers.
-     *
-     * Using the argument `InflectedRoute`, the `fallbacks` method is a shortcut for
-     *    `$routes->connect('/:controller', ['action' => 'index'], ['routeClass' => 'InflectedRoute']);`
-     *    `$routes->connect('/:controller/:action/*', [], ['routeClass' => 'InflectedRoute']);`
-     *
-     * Any route class can be used with this method, such as:
-     * - DashedRoute
-     * - InflectedRoute
-     * - Route
-     * - Or your own route class
-     *
-     * You can remove these routes once you've connected the
-     * routes you want in your application.
-     */
-    $routes->fallbacks('InflectedRoute');
+	/* enable route for /users & /images
+	 * only controllers explicitly enabled for API use will be accessible through your API.
+	 */
+	$routes->resources('Users');
+	$routes->resources('Images');
+
+	/**
+	 * Connect catchall routes for all controllers.
+	 *
+	 * Using the argument `InflectedRoute`, the `fallbacks` method is a shortcut for
+	 *    `$routes->connect('/:controller', ['action' => 'index'], ['routeClass' => 'InflectedRoute']);`
+	 *    `$routes->connect('/:controller/:action/*', [], ['routeClass' => 'InflectedRoute']);`
+	 *
+	 * Any route class can be used with this method, such as:
+	 * - DashedRoute
+	 * - InflectedRoute
+	 * - Route
+	 * - Or your own route class
+	 *
+	 * You can remove these routes once you've connected the
+	 * routes you want in your application.
+	 */
+	$routes->fallbacks('InflectedRoute');
+
 });
 
 /**
